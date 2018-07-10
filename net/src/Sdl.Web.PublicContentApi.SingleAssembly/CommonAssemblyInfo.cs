a8da327150ba766e2a4200dbf57f915d9201b195
Paul Adams <majiccode@gmail.com>
2018-07-10 12:20:22 +0100

    Updated build files

--- net/src/Sdl.Web.PublicContentApi.SingleAssembly/CommonAssemblyInfo.cs
+++ net/src/Sdl.Web.PublicContentApi.SingleAssembly/CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SDL - Bristol")]
 [assembly: AssemblyProduct("Sdl.Web.PublicContentApi")]
-[assembly: AssemblyCopyright("Copyright © SDL - Bristol 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,6 +29,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.*")][assembly: AssemblyVersion("0.0.0.0")]
