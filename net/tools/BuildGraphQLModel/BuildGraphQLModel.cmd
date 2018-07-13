4504633f8b86c4c8ba248d7e36d637f9c82367a4
Paul Adams <majiccode@gmail.com>
2018-07-13 10:29:54 +0100

    Remap namespace and itemType fields to use enums provided by PCA

--- net/tools/BuildGraphQLModel/BuildGraphQLModel.cmd
+++ net/tools/BuildGraphQLModel/BuildGraphQLModel.cmd
@@ -1,3 +1,3 @@
 @echo off
 msbuild BuildGraphQLModel.sln
-bin\Debug\BuildGraphQLModel.exe -ns Sdl.Web.PublicContentApi.ContentModel -e http://localhost:8081 -o ..\..\src\Sdl.Web.PublicContentApi\ContentModel\ContentModel.cs
+bin\Debug\BuildGraphQLModel.exe -ns Sdl.Web.PublicContentApi.ContentModel -e http://localhost:8081/udp/content -o ..\..\src\Sdl.Web.PublicContentApi\ContentModel\ContentModel.cs
