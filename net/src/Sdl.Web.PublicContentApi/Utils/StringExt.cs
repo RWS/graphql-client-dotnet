37aeb56f22a06d002fa958bab4baeec1953306e8
Paul Adams <majiccode@gmail.com>
2018-07-30 15:43:09 +0100

    Added CompentPresentation fragment

--- net/src/Sdl.Web.PublicContentApi/Utils/StringExt.cs
+++ net/src/Sdl.Web.PublicContentApi/Utils/StringExt.cs
@@ -14,7 +14,7 @@ namespace Sdl.Web.PublicContentApi.Utils
 
         public static string Capitialize(this string word)
         {
-            return string.Join(" ", word.Split(' ')
+            return string.Join("", word.Split(' ', '_')
                          .Select(w => w.Trim())
                          .Where(w => w.Length > 0)
                          .Select(w => w.Substring(0, 1).ToUpper() + w.Substring(1).ToLower()));
