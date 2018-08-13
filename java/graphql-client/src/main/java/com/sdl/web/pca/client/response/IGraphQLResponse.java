d266af2d9c19041c3b2df203d562485626724e82
GLOBAL\pmaharana <pradeep2006m@gmail.com>
2018-08-13 19:02:31 +0530

    Added Java docs in IGraphQLResponse interface.

--- java/graphql-client/src/main/java/com/sdl/web/pca/client/response/IGraphQLResponse.java
+++ java/graphql-client/src/main/java/com/sdl/web/pca/client/response/IGraphQLResponse.java
@@ -2,6 +2,11 @@ package com.sdl.web.pca.client.response;
 
 import java.util.List;
 
+/**
+ * This interface represent the Graphql response.
+ * @author
+ * @version 1.0.0
+ */
 public interface IGraphQLResponse {
     Object Data = null;
     List<GraphQLError> Errors = null;
