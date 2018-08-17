5a67332c746991bff3898569c08cca6726dd5402
Rajesh Kumar <rsaminathan@sdl.com>
2018-08-17 16:08:49 +0530

    moved the enums to appropriate folder

--- java/pca-model/src/main/java/com/sdl/web/pca/client/contentmodel/enums/ContentType.java
+++ java/pca-model/src/main/java/com/sdl/web/pca/client/contentmodel/enums/ContentType.java
@@ -1,4 +1,4 @@
-package com.sdl.web.pca.client.contentmodel;
+package com.sdl.web.pca.client.contentmodel.enums;
 
 public enum ContentType {
     RAW, // RAW will perform no conversion and return what is in the Broker
