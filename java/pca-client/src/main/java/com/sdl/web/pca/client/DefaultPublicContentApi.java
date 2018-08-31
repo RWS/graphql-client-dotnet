d0c684bf9c72bdd6c4dd468ff40ccc7333d284f1
GLOBAL\pmaharana <pradeep2006m@gmail.com>
2018-08-31 16:06:21 +0530

    Fixed some changes in pca-model-generator.

--- java/pca-client/src/main/java/com/sdl/web/pca/client/DefaultPublicContentApi.java
+++ java/pca-client/src/main/java/com/sdl/web/pca/client/DefaultPublicContentApi.java
@@ -3,48 +3,23 @@ package com.sdl.web.pca.client;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.sdl.web.pca.client.contentmodel.BinaryComponent;
-import com.sdl.web.pca.client.contentmodel.ClaimValue;
-import com.sdl.web.pca.client.contentmodel.ContentNamespace;
-import com.sdl.web.pca.client.contentmodel.ContentType;
-import com.sdl.web.pca.client.contentmodel.ContextData;
-import com.sdl.web.pca.client.contentmodel.IContextData;
-import com.sdl.web.pca.client.contentmodel.IPagination;
-import com.sdl.web.pca.client.contentmodel.InputClaimValue;
-import com.sdl.web.pca.client.contentmodel.InputItemFilter;
-import com.sdl.web.pca.client.contentmodel.InputPublicationFilter;
-import com.sdl.web.pca.client.contentmodel.InputSortParam;
-import com.sdl.web.pca.client.contentmodel.ItemConnection;
-import com.sdl.web.pca.client.contentmodel.ItemType;
-import com.sdl.web.pca.client.contentmodel.Publication;
-import com.sdl.web.pca.client.contentmodel.PublicationConnection;
-import com.sdl.web.pca.client.contentmodel.PublicationMapping;
-import com.sdl.web.pca.client.contentmodel.TaxonomySitemapItem;
+import com.sdl.web.pca.client.contentmodel.*;
 import com.sdl.web.pca.client.contentmodel.enums.DataModelType;
 import com.sdl.web.pca.client.contentmodel.enums.DcpType;
 import com.sdl.web.pca.client.contentmodel.enums.PageInclusion;
 import com.sdl.web.pca.client.exception.GraphQLClientException;
 import com.sdl.web.pca.client.exception.PublicContentApiException;
 import com.sdl.web.pca.client.request.GraphQLRequest;
-import netscape.javascript.JSObject;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.sdl.web.pca.client.modelserviceplugin.ClaimHelper.createClaim;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class DefaultPublicContentApi implements PublicContentApi {
-    private static final ObjectMapper MAPPER = new ObjectMapper();//.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private GraphQLClient client;
     private Map<String, String> queries = new HashMap<>();
@@ -53,14 +28,14 @@ public class DefaultPublicContentApi implements PublicContentApi {
     private ContextData defaultContextData;
 
     public DefaultPublicContentApi(GraphQLClient graphQLClient) {
-        client = graphQLClient;
-        defaultContextData = new ContextData();
-        defaultContextData.setClaimValues(Collections.EMPTY_LIST);
+        this(graphQLClient, 0);
     }
 
     public DefaultPublicContentApi(GraphQLClient graphQLClient, int requestTimeout) {
-        this(graphQLClient);
+        this.client = graphQLClient;
         this.requestTimeout = requestTimeout;
+        this.defaultContextData = new ContextData();
+        this.defaultContextData.setClaimValues(Collections.EMPTY_LIST);
     }
 
     @Override
@@ -139,38 +114,42 @@ public class DefaultPublicContentApi implements PublicContentApi {
     @Override
     public TaxonomySitemapItem getSitemap(ContentNamespace ns, int publicationId, int descendantLevels,
                                           ContextData contextData) throws PublicContentApiException {
-        //TODO fix: descendsantLevels, contextData is not used in current implementation
+        ContextData mergedData = mergeContextData(defaultContextData, contextData);
         String query = getQueryFor("Sitemap");
-        query += getFragmentFor("RecurseItems");
         query += getFragmentFor("TaxonomyItemFields");
         query += getFragmentFor("TaxonomyPageFields");
+        String recurseItems = getFragmentFor("RecurseItems");
+        query = QueryUtils.expandRecursively(query, recurseItems, descendantLevels);
 
         HashMap<String, Object> variables = new HashMap<>();
         variables.put("namespaceId", ns.getNameSpaceValue());
         variables.put("publicationId", publicationId);
+        variables.put("contextData", mergedData.getClaimValues());
 
         GraphQLRequest graphQLRequest = new GraphQLRequest(query, variables, requestTimeout);
-        return getResultForRequest(graphQLRequest, TaxonomySitemapItem.class, "sitemap");
+        return getResultForRequest(graphQLRequest, TaxonomySitemapItem.class, "/data/sitemap");
     }
 
     @Override
     public TaxonomySitemapItem getSitemapSubtree(ContentNamespace ns, int publicationId, String taxonomyNodeId,
                                                  int descendantLevels, boolean includeAncestors,
                                                  ContextData contextData) throws PublicContentApiException {
-        //TODO fix: descendantLevels, includeAncestors, contextData is not used in current implementatioon
+        ContextData mergedData = mergeContextData(defaultContextData, contextData);
         String query = getQueryFor("SitemapSubtree");
         query += getFragmentFor("TaxonomyItemFields");
-        query += getFragmentFor("RecurseItems");
         query += getFragmentFor("TaxonomyPageFields");
+        String recurseItems = getFragmentFor("RecurseItems");
+        query = QueryUtils.expandRecursively(query, recurseItems, descendantLevels);
 
         HashMap<String, Object> variables = new HashMap<>();
         variables.put("namespaceId", ns.getNameSpaceValue());
         variables.put("publicationId", publicationId);
         variables.put("taxonomyNodeId", taxonomyNodeId);
         variables.put("includeAncestors", includeAncestors);
+        variables.put("contextData", mergedData.getClaimValues());
 
         GraphQLRequest graphQLRequest = new GraphQLRequest(query, variables, requestTimeout);
-        return getResultForRequest(graphQLRequest, TaxonomySitemapItem.class, "sitemapSubtree");
+        return getResultForRequest(graphQLRequest, TaxonomySitemapItem.class, "/data/sitemapSubtree");
     }
 
     @Override
@@ -191,23 +170,48 @@ public class DefaultPublicContentApi implements PublicContentApi {
         variables.put("contextData", mergedData.getClaimValues());
 
         GraphQLRequest graphQLRequest = new GraphQLRequest(query, variables, requestTimeout);
-        JsonNode result = getJsonResult(graphQLRequest, "/data/binaryComponent");
-        try {
-            return MAPPER.treeToValue(result, BinaryComponent.class);
-        } catch (JsonProcessingException e) {
-            throw new PublicContentApiException("Unable map result to BinaryComponent: " + result.toString(), e);
-        }
+        return getResultForRequest(graphQLRequest, BinaryComponent.class, "/data/binaryComponent");
     }
 
     @Override
     public BinaryComponent getBinaryComponent(ContentNamespace ns, int publicationId, String url,
                                               ContextData contextData) throws PublicContentApiException {
-        return null;
-     }
+        ContextData mergedData = mergeContextData(defaultContextData, contextData);
+        String query = getQueryFor("BinaryComponentByUrl");
+        query += getFragmentFor("ItemFields");
+        query += getFragmentFor("BinaryComponentFields");
+        query += getFragmentFor("CustomMetaFields");
+        query = QueryUtils.injectVariantsArgs(query, url);
+        query = QueryUtils.injectCustomMetaFilter(query, null);
+
+        HashMap<String, Object> variables = new HashMap<>();
+        variables.put("namespaceId", ns.getNameSpaceValue());
+        variables.put("publicationId", publicationId);
+        variables.put("url", url);
+        variables.put("contextData", mergedData.getClaimValues());
+
+        GraphQLRequest graphQLRequest = new GraphQLRequest(query, variables, requestTimeout);
+        return getResultForRequest(graphQLRequest, BinaryComponent.class, "/data/binaryComponent");
+    }
 
     @Override
     public BinaryComponent getBinaryComponent(CmUri cmUri, ContextData contextData) throws PublicContentApiException {
-        return null;
+        ContextData mergedData = mergeContextData(defaultContextData, contextData);
+        String query = getQueryFor("BinaryComponentByCmUri");
+        query += getFragmentFor("ItemFields");
+        query += getFragmentFor("BinaryComponentFields");
+        query += getFragmentFor("CustomMetaFields");
+        query = QueryUtils.injectVariantsArgs(query, null);
+        query = QueryUtils.injectCustomMetaFilter(query, null);
+
+        HashMap<String, Object> variables = new HashMap<>();
+        variables.put("namespaceId", cmUri.getNamespaceId());
+        variables.put("publicationId", cmUri.getPublicationId());
+        variables.put("cmUri", cmUri.toString());
+        variables.put("contextData", mergedData.getClaimValues());
+
+        GraphQLRequest graphQLRequest = new GraphQLRequest(query, variables, requestTimeout);
+        return getResultForRequest(graphQLRequest, BinaryComponent.class, "/data/binaryComponent");
     }
 
     @Override
@@ -228,15 +232,11 @@ public class DefaultPublicContentApi implements PublicContentApi {
                 String fragment = itemType.name().substring(0, 1).toUpperCase()
                         + itemType.name().substring(1).toLowerCase() + "Fields";
                 query += getFragmentFor(fragment);
-                if(fragment.equals("PageFields"))
-                    query += getFragmentFor("ComponentPresentationFields");
                 fragmentList += "..." + fragment + "\n";
             }
             // Just a quick and easy way to replace markers in our queries with vars here.
             query = query.replace("@fragmentList", fragmentList);
             query = query.replace("@customMetaFilter", "\"" + customMetaFilter + "\"");
-            query = query.replace("@customMetaArgs", customMetaFilter );
-            query = query.replace("@renderContentArgs",  customMetaFilter );
         }
 
         InputClaimValue[] inputClaimValues = new InputClaimValue[0];
@@ -248,7 +248,7 @@ public class DefaultPublicContentApi implements PublicContentApi {
         variables.put("contextData", inputClaimValues);
 
         GraphQLRequest graphQLRequest = new GraphQLRequest(query, variables, requestTimeout);
-        return getResultForRequest(graphQLRequest, ItemConnection.class, "items");
+        return getResultForRequest(graphQLRequest, ItemConnection.class);
     }
 
     @Override
@@ -296,22 +296,6 @@ public class DefaultPublicContentApi implements PublicContentApi {
         return null;
     }
 
-//    @Override
-//    public <T> T executeSiteMap(Page page, Class<T> clazz) throws PublicContentApiException {
-//        String query = getQueryFor(SITEMAP);
-//        query += getQueryFor("RecurseItems");
-//        query += getQueryFor("TaxonomyItemFields");
-//        query += getQueryFor("TaxonomyPageFields");
-//
-//        HashMap<String, Object> variables = new HashMap<>();
-//        variables.put("namespaceId", page.getNamespaceId());
-//        variables.put("publicationId", page.getPublicationId());
-//
-//        GraphQLRequest graphQLRequest = new GraphQLRequest(query, variables, requestTimeout);
-//        return getResultForRequest(graphQLRequest, clazz);
-//    }
-
-
     private String getQueryFor(String queryName) throws PublicContentApiException {
         return queries.computeIfAbsent(queryName,
                 s -> loadQueryFromResourcefile("queries/" + s));
@@ -331,19 +315,23 @@ public class DefaultPublicContentApi implements PublicContentApi {
         }
     }
 
-    private <T> T getResultForRequest(GraphQLRequest request, Class<T> clazz, String subroot) throws PublicContentApiException {
+    private <T> T getResultForRequest(GraphQLRequest request, Class<T> clazz) throws PublicContentApiException {
         try {
             String contentQuery = client.execute(request);
-
-            JsonParser parser = new JsonParser();
-            JsonObject jsonObject = (JsonObject)parser.parse(contentQuery);
-            JsonObject dataObject = jsonObject.getAsJsonObject("data").getAsJsonObject(subroot);
-
-            return MAPPER.readValue(dataObject.toString(), clazz);
+            return MAPPER.readValue(contentQuery, clazz);
         } catch (GraphQLClientException e) {
             throw new PublicContentApiException("Unable to execute query: " + request, e);
         } catch (IOException e) {
             throw new PublicContentApiException("Unable to deserialize result for query " + request, e);
+        }
+    }
+
+    private <T> T getResultForRequest(GraphQLRequest request, Class<T> clazz, String path) throws PublicContentApiException {
+        JsonNode result = getJsonResult(request, path);
+        try {
+            return MAPPER.treeToValue(result, clazz);
+        } catch (JsonProcessingException e) {
+            throw new PublicContentApiException("Unable map result to " + clazz.getName() + ": " + result.toString(), e);
         }
     }
 
@@ -370,10 +358,6 @@ public class DefaultPublicContentApi implements PublicContentApi {
         ContextData result = new ContextData();
         result.setClaimValues(merged);
         return result;
-    }
-
-    public GraphQLClient getGraphQLClient() {
-        return client;
     }
 
     public void setGraphQLClient(GraphQLClient client) {
--- java/pca-model/src/main/java/com/sdl/web/pca/client/contentmodel/BinaryVariantEdge.java
+++ java/pca-model/src/main/java/com/sdl/web/pca/client/contentmodel/BinaryVariantEdge.java
@@ -1,27 +1,28 @@
 package com.sdl.web.pca.client.contentmodel;
 
-
-/**
-*An edge in a connection
-*/
-public class BinaryVariantEdge {
+/// <summary>
+	/// An edge in a connection
+	/// </summary>
+	public class BinaryVariantEdge
+	{
 		private BinaryVariant node;
 		private String cursor;
 
+		 public BinaryVariant getNode()
+		 {
+			 return node;
+		 }
+		 public void setNode(BinaryVariant node)
+		 {
+			 this.node = node;
+		 }
 
-		public BinaryVariant getNode(){
-			return node;
-		}
-		public void setNode(BinaryVariant node){
-			this.node = node;
-		}
-
-
-		public String getCursor(){
-			return cursor;
-		}
-		public void setCursor(String cursor){
-			this.cursor = cursor;
-		}
-	
-}
+		 public String getCursor()
+		 {
+			 return cursor;
+		 }
+		 public void setCursor(String cursor)
+		 {
+			 this.cursor = cursor;
+		 }
+	}
