09bd81f9e2c4d396feefa3f4dff67bd36c448525
GLOBAL\pmaharana <pradeep2006m@gmail.com>
2018-09-10 16:25:33 +0530

    Removed assertNotNull and change Object to BinaryComponent

--- java/pca-integration-tests/src/test/java/com/sdl/web/pca/client/GraphQLClientTest.java
+++ java/pca-integration-tests/src/test/java/com/sdl/web/pca/client/GraphQLClientTest.java
@@ -53,64 +53,61 @@ public class GraphQLClientTest {
 
     @Test
     public void getBinaryComponentById() throws Exception {
-        Object result = publicContentApi.getBinaryComponent(ContentNamespace.Sites, 8, 756,
+        BinaryComponent result = publicContentApi.getBinaryComponent(ContentNamespace.Sites, 8, 756,
                 null);
-        assertNotNull(result);
 
         assertEquals(BinaryComponent.class, result.getClass());
-        assertEquals("b4e5c7c4-f04a-3d6d-898f-5886d0f648bd", ((BinaryComponent) result).getId());
-        assertEquals(1, ((BinaryComponent) result).getNamespaceId());
-        assertEquals(8, ((BinaryComponent) result).getPublicationId());
-        assertEquals("tcd:pub[8]/componentmeta[756]", ((BinaryComponent) result).getTitle());
-        assertEquals("2018-06-25T14:01:40.007+05:30", ((BinaryComponent) result).getLastPublishDate());
-        assertEquals("2017-01-24T09:31:46.643+05:30", ((BinaryComponent) result).getCreationDate());
-        assertEquals(756, ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getBinaryId());
-        assertEquals("/media/balloons_tcm8-756.jpg", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getPath());
-        assertEquals("/media/balloons_tcm8-756.jpg", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getUrl());
-        assertEquals("http://10.100.93.148:8081/udp/content/binary/1/8/756", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getDownloadUrl());
-        assertEquals("[#def#]", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getVariantId());
+        assertEquals("b4e5c7c4-f04a-3d6d-898f-5886d0f648bd", result.getId());
+        assertEquals(1, result.getNamespaceId());
+        assertEquals(8, result.getPublicationId());
+        assertEquals("tcd:pub[8]/componentmeta[756]", result.getTitle());
+        assertEquals("2018-06-25T14:01:40.007+05:30", result.getLastPublishDate());
+        assertEquals("2017-01-24T09:31:46.643+05:30", result.getCreationDate());
+        assertEquals(756, result.getVariants().getEdges().get(0).getNode().getBinaryId());
+        assertEquals("/media/balloons_tcm8-756.jpg", result.getVariants().getEdges().get(0).getNode().getPath());
+        assertEquals("/media/balloons_tcm8-756.jpg", result.getVariants().getEdges().get(0).getNode().getUrl());
+        assertEquals("http://10.100.93.148:8081/udp/content/binary/1/8/756", result.getVariants().getEdges().get(0).getNode().getDownloadUrl());
+        assertEquals("[#def#]", result.getVariants().getEdges().get(0).getNode().getVariantId());
 
     }
 
     @Test
     public void getBinaryComponentByUrl() throws Exception {
-        Object result = publicContentApi.getBinaryComponent(ContentNamespace.Sites, 8,
+        BinaryComponent result = publicContentApi.getBinaryComponent(ContentNamespace.Sites, 8,
                 "/media/balloons_tcm8-756.jpg", null);
-        assertNotNull(result);
 
         assertEquals(BinaryComponent.class, result.getClass());
-        assertEquals("b4e5c7c4-f04a-3d6d-898f-5886d0f648bd", ((BinaryComponent) result).getId());
-        assertEquals(1, ((BinaryComponent) result).getNamespaceId());
-        assertEquals(8, ((BinaryComponent) result).getPublicationId());
-        assertEquals("tcd:pub[8]/componentmeta[756]", ((BinaryComponent) result).getTitle());
-        assertEquals("2018-06-25T14:01:40.007+05:30", ((BinaryComponent) result).getLastPublishDate());
-        assertEquals("2017-01-24T09:31:46.643+05:30", ((BinaryComponent) result).getCreationDate());
-        assertEquals(756, ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getBinaryId());
-        assertEquals("/media/balloons_tcm8-756.jpg", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getPath());
-        assertEquals("/media/balloons_tcm8-756.jpg", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getUrl());
-        assertEquals("http://10.100.93.148:8081/udp/content/binary/1/8/756", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getDownloadUrl());
-        assertEquals("[#def#]", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getVariantId());
+        assertEquals("b4e5c7c4-f04a-3d6d-898f-5886d0f648bd", result.getId());
+        assertEquals(1, result.getNamespaceId());
+        assertEquals(8, result.getPublicationId());
+        assertEquals("tcd:pub[8]/componentmeta[756]", result.getTitle());
+        assertEquals("2018-06-25T14:01:40.007+05:30", result.getLastPublishDate());
+        assertEquals("2017-01-24T09:31:46.643+05:30", result.getCreationDate());
+        assertEquals(756, result.getVariants().getEdges().get(0).getNode().getBinaryId());
+        assertEquals("/media/balloons_tcm8-756.jpg", result.getVariants().getEdges().get(0).getNode().getPath());
+        assertEquals("/media/balloons_tcm8-756.jpg", result.getVariants().getEdges().get(0).getNode().getUrl());
+        assertEquals("http://10.100.93.148:8081/udp/content/binary/1/8/756", result.getVariants().getEdges().get(0).getNode().getDownloadUrl());
+        assertEquals("[#def#]", result.getVariants().getEdges().get(0).getNode().getVariantId());
 
     }
 
     @Test
     public void getBinaryCompoonentByCmUri() throws Exception {
-        Object result = publicContentApi.getBinaryComponent(new CmUri("tcm:8-756-16"), new ContextData());
-        assertNotNull(result);
+        BinaryComponent result = publicContentApi.getBinaryComponent(new CmUri("tcm:8-756-16"), new ContextData());
 
         assertEquals(BinaryComponent.class, result.getClass());
-        assertEquals("b4e5c7c4-f04a-3d6d-898f-5886d0f648bd", ((BinaryComponent) result).getId());
-        assertEquals(756, ((BinaryComponent) result).getItemId());
-        assertEquals(1, ((BinaryComponent) result).getNamespaceId());
-        assertEquals(8, ((BinaryComponent) result).getPublicationId());
-        assertEquals("tcd:pub[8]/componentmeta[756]", ((BinaryComponent) result).getTitle());
-        assertEquals("2018-06-25T14:01:40.007+05:30", ((BinaryComponent) result).getLastPublishDate());
-        assertEquals("2017-01-24T09:31:46.643+05:30", ((BinaryComponent) result).getCreationDate());
-        assertEquals(756, ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getBinaryId());
-        assertEquals("/media/balloons_tcm8-756.jpg", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getPath());
-        assertEquals("/media/balloons_tcm8-756.jpg", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getUrl());
-        assertEquals("http://10.100.93.148:8081/udp/content/binary/1/8/756", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getDownloadUrl());
-        assertEquals("[#def#]", ((BinaryComponent) result).getVariants().getEdges().get(0).getNode().getVariantId());
+        assertEquals("b4e5c7c4-f04a-3d6d-898f-5886d0f648bd", result.getId());
+        assertEquals(756, result.getItemId());
+        assertEquals(1, result.getNamespaceId());
+        assertEquals(8, result.getPublicationId());
+        assertEquals("tcd:pub[8]/componentmeta[756]", result.getTitle());
+        assertEquals("2018-06-25T14:01:40.007+05:30", result.getLastPublishDate());
+        assertEquals("2017-01-24T09:31:46.643+05:30", result.getCreationDate());
+        assertEquals(756, result.getVariants().getEdges().get(0).getNode().getBinaryId());
+        assertEquals("/media/balloons_tcm8-756.jpg", result.getVariants().getEdges().get(0).getNode().getPath());
+        assertEquals("/media/balloons_tcm8-756.jpg", result.getVariants().getEdges().get(0).getNode().getUrl());
+        assertEquals("http://10.100.93.148:8081/udp/content/binary/1/8/756", result.getVariants().getEdges().get(0).getNode().getDownloadUrl());
+        assertEquals("[#def#]", result.getVariants().getEdges().get(0).getNode().getVariantId());
     }
 
     @Test
@@ -261,39 +258,30 @@ public class GraphQLClientTest {
     @Test
     public void executeResolveBinaryLink() {
         String result = publicContentApi.resolveBinaryLink(ContentNamespace.Sites, 8, 756, "[#def#]", true);
-        assertNotNull(result);
-
         assertEquals("/media/balloons_tcm8-756.jpg", result);
     }
 
     @Test
     public void executeResolvePageLink() {
         String result = publicContentApi.resolvePageLink(ContentNamespace.Sites, 8, 4447, true);
-        assertNotNull(result);
-
         assertEquals("/system/include/content-tools.html", result);
     }
 
     @Test
     public void executeResolveComponentLink() {
         String result = publicContentApi.resolveComponentLink(ContentNamespace.Sites, 8, 3286, 640, 3292, true);
-        assertNotNull(result);
-
         assertEquals("/articles/all-articles.html", result);
     }
 
     @Test
     public void executeResolveDynamicComponentLink() {
         String result = publicContentApi.resolveDynamicComponentLink(ContentNamespace.Sites, 1082, 4569, 4565, 9195, true);
-        assertNotNull(result);
-
         assertEquals("/example-legacy/articles/news/news1.html", result);
     }
 
     @Test
     public void executegetPulbicationMapping() {
         PublicationMapping result = publicContentApi.getPublicationMapping(ContentNamespace.Sites, "http://localhost:8882/");
-        assertNotNull(result);
 
         assertEquals(PublicationMapping.class, result.getClass());
         assertEquals(5, result.getPublicationId());
@@ -306,7 +294,6 @@ public class GraphQLClientTest {
 
     @Test
     public void executeGetPublication() {
-        assertNotNull(publicContentApi.getPublication(ContentNamespace.Sites, 8, new ContextData(), ""));
         Publication result = publicContentApi.getPublication(ContentNamespace.Sites, 8, new ContextData(), "");
 
         assertEquals(Publication.class, result.getClass());
@@ -324,9 +311,7 @@ public class GraphQLClientTest {
     public void executeGetPublications() {
         Pagination pagination = new Pagination();
         pagination.setFirst(1);
-        assertNotNull(publicContentApi.getPublications(ContentNamespace.Sites, pagination, null, new ContextData(), ""));
         PublicationConnection result = publicContentApi.getPublications(ContentNamespace.Sites, pagination, null, new ContextData(), "");
-        assertNotNull(result.getEdges().get(0).getNode());
 
         assertEquals(PublicationConnection.class, result.getClass());
         assertEquals("MQ==", result.getEdges().get(0).getCursor());
