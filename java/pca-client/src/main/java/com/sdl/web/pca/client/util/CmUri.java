e6c57cb4784f0b4603966c3a7e0d0a8b94f309e2
Mykola Bilenko <mbilenko@sdl.com>
2018-09-19 18:56:11 +0300

    TSI-3282 refactored CmUri class

--- java/pca-client/src/main/java/com/sdl/web/pca/client/util/CmUri.java
+++ java/pca-client/src/main/java/com/sdl/web/pca/client/util/CmUri.java
@@ -1,17 +1,17 @@
 package com.sdl.web.pca.client.util;
 
+import com.google.common.base.Strings;
 import org.slf4j.Logger;
 
-import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.StringTokenizer;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import static com.sdl.web.pca.client.util.ItemTypes.COMPONENT;
 import static com.sdl.web.pca.client.util.ItemTypes.IDNULL;
-import static com.sdl.web.pca.client.util.ItemTypes.NULL;
-import static java.util.Comparator.comparing;
+import static com.sdl.web.pca.client.util.ItemTypes.getById;
+import static java.lang.Integer.valueOf;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -19,116 +19,50 @@ import static org.slf4j.LoggerFactory.getLogger;
  * <p>
  * This object is a helper object used to parse and create Content Manager (TCM or ISH) URIs.
  */
-public class CmUri implements Comparable<CmUri> {
-
+public class CmUri {
+    private static final Logger LOG = getLogger(CmUri.class);
     private static final String SEPARATOR = "-";
     private static final String URI_SEPARATOR = ":";
-    private static final Namespace DEFAULT_NAMESPACE = Namespace.SITES;
-    private static final Logger LOG = getLogger(CmUri.class);
-
-
-    /**
-     * Null URI. Defaulting to TCM namespace.
-     */
-    public static final CmUri NULL_URI = new CmUri(DEFAULT_NAMESPACE.getName(), 0, 0, 0, 0);
+    private static final Pattern CM_URI_PATTERN = Pattern
+            .compile("^(?<namespace>[a-zA-Z]+):(?<pubId>\\d+)-(?<itemId>\\d+)(?:-(?<itemType>\\d+))?(?:-v(?<version>\\d+))?$");
 
     private Namespace namespace;
-    private int itemType;
-    private int itemId;
     private int pubId;
-    private int version;
-
-    /**
-     * Create a new URI based on namespace, publication Id, item Id, item type and version.
-     *
-     * @param namespace     namespace for this URI.
-     * @param publicationId publicationId for this URI.
-     * @param itemId        itemId for this URI.
-     * @param itemType      itemType for this URI.
-     * @param version       version for this URI.
-     */
-    public CmUri(String namespace, int publicationId, int itemId, int itemType, int version) {
-        this.namespace = Namespace.valueByName(namespace.toLowerCase());
-        this.itemType = itemType;
-        this.itemId = itemId;
-        this.pubId = publicationId;
-        this.version = version;
-    }
+    private int itemId;
+    private int itemType;
+    private Optional<Integer> version = Optional.empty();
 
     /**
      * Create a new URI from a string.
      *
      * @param uri String containing the URI to be parsed.
-     * @throws ParseException Thrown if the String does not contain a valid <code>CMURI</code>
      */
-    public CmUri(String uri) throws ParseException {
-        this.namespace = null;
-        this.itemType = NULL.getValue();
-        this.itemId = IDNULL.getValue();
-        this.pubId = IDNULL.getValue();
-        this.version = IDNULL.getValue();
+    public CmUri(String uri) {
         this.load(uri);
     }
 
-    /**
-     * This method is used to create a <code>CMURI</code> based on a String URI.
-     *
-     * @param uriString String representation of the URI.
-     * @throws ParseException Thrown if the String does not contain a valid <code>CMURI</code>
-     */
-    protected void load(String uriString) throws ParseException {
-        if (uriString != null) {
-            if (!uriString.contains(URI_SEPARATOR)) {
-                throw new ParseException("Separator character in uri [" + uriString +
-                        "] is not found. Cannot determine namespace.", 0);
-            }
-
-            if (namespace == null) {
-                // in case namespace is not already set by the subclasses
-                this.namespace = Namespace.valueByName(uriString.substring(0, uriString.indexOf(URI_SEPARATOR)));
-            }
-            int namespacePlusColonLength = namespace.getName().length() + 1;
-            int uriStringLength = uriString.length();
-            int currentPosition = uriStringLength; //current position during parsing, used for reporting errors
-
-            String uri = uriString.substring(namespacePlusColonLength, uriStringLength);
-            StringTokenizer st = new StringTokenizer(uri, SEPARATOR);
-            if (st.countTokens() < 2) {
-                throw new ParseException("URI string " + uriString + " does not contain enough ID's", currentPosition);
-            }
-            try {
-                String token = st.nextToken();
-                currentPosition += token.length();
-                this.pubId = Integer.parseInt(token);
-
-                token = st.nextToken();
-                currentPosition += token.length();
-                this.itemId = Integer.parseInt(token);
-
-                if (!st.hasMoreTokens()) { //if we only have two tokens, then assume component
-                    this.itemType = COMPONENT.getValue();
-                } else {
-                    token = st.nextToken();
-                    currentPosition += token.length();
-                    if (!token.startsWith("v")) {  //item type for non-component
-                        this.itemType = Integer.parseInt(token);
-                    } else { //version for component
-                        this.version = Integer.parseInt(token.substring(1, token.length()));
-                        this.itemType = COMPONENT.getValue();
-                    }
-
-                    if (st.hasMoreTokens()) { //version for non component
-                        token = st.nextToken();
-                        currentPosition += token.length();
-                        this.version = Integer.parseInt(token.substring(1, token.length()));
-                    }
-                }
-            } catch (NumberFormatException e) {
-                throw new ParseException("Invalid ID (not an integer) in URI string " + uriString, currentPosition);
-            }
-        } else {
-            throw new ParseException("Invalid CMURI String, string cannot be null", 0);
+    private void load(String uriString) {
+        Matcher matcher = CM_URI_PATTERN.matcher(uriString);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Unable to parse CMURI: " + uriString);
         }
+
+        this.namespace = Namespace.valueByName(matcher.group("namespace"));
+        this.pubId = valueOf(matcher.group("pubId"));
+        this.itemId = valueOf(matcher.group("itemId"));
+        String itemTypeStr = matcher.group("itemType");
+        if (Strings.isNullOrEmpty(itemTypeStr)) {
+            itemType = ItemTypes.COMPONENT.getValue();
+        } else {
+            // assign itemType with validation
+            itemType = getById(valueOf(itemTypeStr)).getValue();
+        }
+
+        String versionStr = matcher.group("version");
+        if (!Strings.isNullOrEmpty(versionStr)) {
+            version = Optional.of(valueOf(versionStr));
+        }
+
     }
 
     /**
@@ -137,11 +71,11 @@ public class CmUri implements Comparable<CmUri> {
      * @return A string representation of this <code>CMURI</code>.
      */
     public String toString() {
-        return this.namespace.getName() + URI_SEPARATOR + this.pubId + SEPARATOR + this.itemId + SEPARATOR + this.itemType;
-    }
-
-    public void setNamespace(String namespace) {
-        this.namespace = Namespace.valueByName(namespace);
+        String versionStr = version.isPresent()
+                ? "-v" + getVersion()
+                : "";
+        return this.namespace.getName() + URI_SEPARATOR + this.pubId + SEPARATOR + this.itemId + SEPARATOR
+                + this.itemType + versionStr;
     }
 
     public String getNamespace() {
@@ -162,38 +96,12 @@ public class CmUri implements Comparable<CmUri> {
     }
 
     /**
-     * Sets the itemType.
-     *
-     * @param itemType The new itemType.
-     */
-    public void setItemType(int itemType) {
-        if (isNullUri() && itemType != 0) {
-            throw new IllegalStateException("Changing the item type of the NULL_URI is not allowed!!!");
-        }
-
-        this.itemType = itemType;
-    }
-
-    /**
      * Get the item Id for this URI.
      *
      * @return The item Id.
      */
     public int getItemId() {
         return this.itemId;
-    }
-
-    /**
-     * Sets the itemId.
-     *
-     * @param itemId The new itemId
-     */
-    public void setItemId(int itemId) {
-        if (isNullUri() && itemId != 0) {
-            throw new IllegalStateException("Changing the item id '" + itemId + "' of the NULL_URI is not allowed!!!");
-        }
-
-        this.itemId = itemId;
     }
 
     /**
@@ -206,46 +114,12 @@ public class CmUri implements Comparable<CmUri> {
     }
 
     /**
-     * Sets the publicationId.
-     *
-     * @param thePubId The new publicationId.
-     */
-    public void setPublicationId(int thePubId) {
-        if (isNullUri() && thePubId != 0) {
-            throw new IllegalStateException("Changing the publication id '" + thePubId + "' of the NULL_URI is not allowed!!!");
-        }
-        this.pubId = thePubId;
-    }
-
-    /**
      * Get the version for this URI.
      *
      * @return The version.
      */
     public int getVersion() {
-        return this.version;
-    }
-
-    /**
-     * Sets the version.
-     *
-     * @param version the new version.
-     */
-    public void setVersion(int version) {
-        if (isNullUri() && version != 0) {
-            throw new IllegalStateException("Changing the version of the NULL_URI is not allowed!!!");
-        }
-
-        this.version = version;
-    }
-
-    /**
-     * Check if this object is NULL_URI.
-     *
-     * @return true if this object is NULL_URI, false otherwise.
-     */
-    protected boolean isNullUri() {
-        return this == NULL_URI;
+        return version.orElse(IDNULL.getValue());
     }
 
     /**
@@ -257,7 +131,7 @@ public class CmUri implements Comparable<CmUri> {
     private boolean equals(String uri) {
         try {
             return this.equals(new CmUri(uri));
-        } catch (ParseException e) {
+        } catch (IllegalArgumentException e) {
             LOG.debug("Unable to parse uri: {}. Assuming it doesn't equal to {}", uri, this.toString());
             return false;
         }
@@ -270,10 +144,11 @@ public class CmUri implements Comparable<CmUri> {
      * @return a boolean indicating if the other URI is equal.
      */
     private boolean equals(CmUri uri) {
-        return stringEquals(this.getNamespace(), uri.getNamespace())
+        return this.getNamespace() == uri.getNamespace()
                 && this.getItemType() == uri.getItemType()
                 && this.getItemId() == uri.getItemId()
-                && this.getPublicationId() == uri.getPublicationId();
+                && this.getPublicationId() == uri.getPublicationId()
+                && this.getVersion() == uri.getVersion();
     }
 
     /**
@@ -300,18 +175,9 @@ public class CmUri implements Comparable<CmUri> {
         return this.toString().hashCode();
     }
 
-    @Override
-    public int compareTo(CmUri other) {
-        return comparing(CmUri::getNamespace)
-                .thenComparing(CmUri::getPublicationId)
-                .thenComparing(CmUri::getItemType)
-                .thenComparing(CmUri::getItemId).compare(this, other);
-    }
-
-    public static boolean stringEquals(String str1, String str2) {
-        return Objects.isNull(str1) ? Objects.isNull(str2) : str1.equals(str2);
-    }
-
+    /**
+     * Represents namespace values.
+     */
     public enum Namespace {
         SITES("tcm", 1),
         DOCS("ish", 2);
@@ -343,10 +209,9 @@ public class CmUri implements Comparable<CmUri> {
         public static Namespace valueByName(String name) {
             Namespace result = namespaceByName.get(name);
             if (result == null) {
-                throw new IllegalArgumentException("Unable to resolve namespace " + name);
+                throw new IllegalArgumentException("Unable to resolve namespace '" + name + "'");
             }
             return result;
         }
     }
-
 }
