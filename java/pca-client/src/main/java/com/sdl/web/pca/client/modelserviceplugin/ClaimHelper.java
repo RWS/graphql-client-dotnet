b8598df52f8c2b6fc7b66e4455d27e61bf685056
Mykola Bilenko <mbilenko@sdl.com>
2018-11-12 11:36:17 +0200

    TSI-3576 changes according to pr comment

--- java/pca-client/src/main/java/com/sdl/web/pca/client/modelserviceplugin/ClaimHelper.java
+++ java/pca-client/src/main/java/com/sdl/web/pca/client/modelserviceplugin/ClaimHelper.java
@@ -12,66 +12,58 @@ import com.sdl.web.pca.client.contentmodel.generated.ClaimValueType;
 public class ClaimHelper {
 
     public static ClaimValue createClaim(ModelServiceLinkRendering linkRendering) {
-        ClaimValue value = new ClaimValue();
-        value.setType(ClaimValueType.BOOLEAN);
-        value.setUri(ModelServiceClaimUris.MODEL_SERVICE_LINK_RENDERING);
-        value.setValue(linkRendering == ModelServiceLinkRendering.RELATIVE ? "true" : "false");
-        return value;
+        return createClaimInternal(ModelServiceClaimUris.MODEL_SERVICE_LINK_RENDERING,
+                linkRendering == ModelServiceLinkRendering.RELATIVE ? "true" : "false",
+                ClaimValueType.BOOLEAN);
     }
 
     public static ClaimValue createClaim(TcdlLinkRendering linkRendering) {
-        ClaimValue value = new ClaimValue();
-        value.setType(ClaimValueType.BOOLEAN);
-        value.setUri(ModelServiceClaimUris.TCDL_LINK_RENDERING);
-        value.setValue(linkRendering == TcdlLinkRendering.RELATIVE ? "true" : "false");
-        return value;
+        return createClaimInternal(ModelServiceClaimUris.TCDL_LINK_RENDERING,
+                linkRendering == TcdlLinkRendering.RELATIVE ? "true" : "false",
+                ClaimValueType.BOOLEAN);
     }
 
     public static ClaimValue createClaimTcdlLinkUrlPrefix(String urlPrefix) {
-        ClaimValue value = new ClaimValue();
-        value.setType(ClaimValueType.STRING);
-        value.setUri(ModelServiceClaimUris.TCDL_LINK_URL_PREFIX);
-        value.setValue(urlPrefix);
-        return value;
+        return createClaimInternal(ModelServiceClaimUris.TCDL_LINK_URL_PREFIX,
+                urlPrefix,
+                ClaimValueType.STRING);
     }
 
     public static ClaimValue createClaimTcdlBinaryLinkUrlPrefix(String urlPrefix) {
-        ClaimValue value = new ClaimValue();
-        value.setType(ClaimValueType.STRING);
-        value.setUri(ModelServiceClaimUris.TCDL_BINARY_LINK_URL_PREFIX);
-        value.setValue(urlPrefix);
-        return value;
+        return createClaimInternal(ModelServiceClaimUris.TCDL_BINARY_LINK_URL_PREFIX,
+                urlPrefix,
+                ClaimValueType.STRING);
     }
 
     public static ClaimValue createClaim(ContentType contentType) {
-        ClaimValue value = new ClaimValue();
-        value.setType(ClaimValueType.STRING);
-        value.setUri(ModelServiceClaimUris.CONTENT_TYPE);
-        value.setValue(contentType.name());
-        return value;
+        return createClaimInternal(ModelServiceClaimUris.CONTENT_TYPE,
+                contentType.name(),
+                ClaimValueType.STRING);
     }
 
     public static ClaimValue createClaim(DataModelType dataModelType) {
-        ClaimValue value = new ClaimValue();
-        value.setType(ClaimValueType.STRING);
-        value.setUri(ModelServiceClaimUris.MODEL_TYPE);
-        value.setValue(dataModelType.name());
-        return value;
+        return createClaimInternal(ModelServiceClaimUris.MODEL_TYPE,
+                dataModelType.name(),
+                ClaimValueType.STRING);
     }
 
     public static ClaimValue createClaim(PageInclusion pageInclusion) {
-        ClaimValue value = new ClaimValue();
-        value.setType(ClaimValueType.STRING);
-        value.setUri(ModelServiceClaimUris.PAGE_INCLUDE_REGIONS);
-        value.setValue(pageInclusion.name());
-        return value;
+        return createClaimInternal(ModelServiceClaimUris.PAGE_INCLUDE_REGIONS,
+                pageInclusion.name(),
+                ClaimValueType.STRING);
     }
 
     public static ClaimValue createClaim(DcpType dcpType) {
-        ClaimValue value = new ClaimValue();
-        value.setType(ClaimValueType.STRING);
-        value.setUri(ModelServiceClaimUris.ENTITY_DCP_TYPE);
-        value.setValue(dcpType.name());
-        return value;
+        return createClaimInternal(ModelServiceClaimUris.ENTITY_DCP_TYPE,
+                dcpType.name(),
+                ClaimValueType.STRING);
+    }
+
+    private static ClaimValue createClaimInternal(String uri, String value, ClaimValueType type) {
+        ClaimValue claim = new ClaimValue();
+        claim.setType(type);
+        claim.setUri(uri);
+        claim.setValue(value);
+        return claim;
     }
 }
